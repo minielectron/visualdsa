62d19387ef35d47587611b4a6bf667de272ae46c
Prakash sharma <prakash.sharma1@telekom-digital.com>
2025-03-19 23:43:45 +0530

    Add more sorting algorithms implementation

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ import TreesPage from './pages/dataStructures/TreesPage';
 import HashTablesPage from './pages/dataStructures/HashTablesPage';
 import GraphsPage from './pages/dataStructures/GraphsPage';
 import HeapsPage from './pages/dataStructures/HeapsPage';
+import BubbleSort from './pages/algorithms/BubbleSort';
+import SelectionSort from './pages/algorithms/SelectionSort';
+import InsertionSort from './pages/algorithms/InsertionSort';
+import BinarySearch from './pages/algorithms/BinarySearch';
+import LinearSearch from './pages/algorithms/LinearSearch';
 
 const AppContainer = styled.div`
   min-height: 100vh;
@@ -44,7 +49,18 @@ function App() {
             <Route path="/data-structures/hash-tables" element={<HashTablesPage />} />
             <Route path="/data-structures/graphs" element={<GraphsPage />} />
             <Route path="/data-structures/heaps" element={<HeapsPage />} />
-            {/* More data structure routes will be added as we implement them */}
+            
+            {/* Algorithm routes */}
+            {/* Sorting algorithms */}
+            <Route path="/algorithms/sorting/bubble" element={<BubbleSort />} />
+            <Route path="/algorithms/sorting/selection" element={<SelectionSort />} />
+            <Route path="/algorithms/sorting/insertion" element={<InsertionSort />} />
+            
+            {/* Searching algorithms */}
+            <Route path="/algorithms/searching/binary" element={<BinarySearch />} />
+            <Route path="/algorithms/searching/linear" element={<LinearSearch />} />
+            
+            {/* More routes will be added as we implement them */}
           </Routes>
         </MainContent>
       </AppContainer>
